c14063b8f0459ee7bdad28139583490149922379
austintian1-2025 <austin.tian1@gmail.com>
2025-12-10 15:10:13 -0500

    test

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
